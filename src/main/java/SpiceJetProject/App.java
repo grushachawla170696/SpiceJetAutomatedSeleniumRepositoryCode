8e63ee5cfdf58ecd2efd3f5ee33052711052f0ef
grushachawla170696 <93521618+grushachawla170696@users.noreply.github.com>
2021-11-02 16:32:21 +0530

    Update App.java

--- src/main/java/SpiceJetProject/App.java
+++ src/main/java/SpiceJetProject/App.java
@@ -52,7 +52,11 @@ public class App
 		System.out.println("Select a To Return date");
 		explicitWait(Constants.ReturnDate);
 		Thread.sleep(2000);//for observing the action
-
+		System.out.println("select passengers");
+		driver.findElement(By.xpath("//*[contains(text(),'Passengers')]")).click();
+		Thread.sleep(2000);//for observing the action
+		driver.findElement(By.xpath("//body/div[@id='react-root']/div[@id='main-container']/div[@class='css-1dbjc4n r-1niwhzg r-13awgt0 r-1jgb5lz r-r0h9e2 r-13qz1uu']/div[@class='css-1dbjc4n r-14lw9ot']/div[@class='css-1dbjc4n r-14lw9ot r-z2wwpe r-vgw6uq r-156q2ks r-urutk0 r-8uuktl r-136ojw6']/div[@class='css-1dbjc4n r-1pcd2l5 r-1uwte3a r-m611by r-bnwqim']/div[@class='css-1dbjc4n r-18u37iz r-19h5ruw r-184en5c']/div[@class='css-1dbjc4n']/div/div[@class='css-1dbjc4n r-14lw9ot r-z2wwpe r-1rjd0u6 r-1g94qm0 r-h3f8nf r-u8s1d r-13qz1uu r-8fdsdq']/div[@class='css-1dbjc4n r-150rngu r-eqz5dr r-16y2uox r-1wbh5a2 r-11yh6sk r-1rnoaur r-1sncvnh']/div[@class='css-1dbjc4n']/div[@class='css-1dbjc4n r-k8qxaj r-d9fdf6']/div[1]/div[2]/div[3]/*[1]")).click();
+		Thread.sleep(2000);//for observing the action
 		System.out.println("Select GBP currency from the drop down");
 		driver.findElement(By.xpath("//*[contains(text(),'Currency')]")).click();
 		Thread.sleep(2000);//for observing the action
